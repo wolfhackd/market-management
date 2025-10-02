4d065fc66830b81f1d2110f70b41b782c13deace
Mauro Leal <mauro.costa.12.j@hotmail.com>
2025-10-02 11:00:10 -0300

    docs: produto docs swagger

--- src/modules/clientes/cliente.routes.ts
+++ src/modules/clientes/cliente.routes.ts
@@ -1,7 +1,33 @@
 import type { FastifyInstance } from 'fastify';
 import { criarCliente, listarClientes } from './cliente.controller.js';
+import { clienteSchema } from './cliente.schema.js';
 
 export async function clientesRoutes(fastify: FastifyInstance) {
-  fastify.post('/criarCliente', criarCliente);
-  fastify.get('/listarClientes', listarClientes);
+  fastify.post(
+    '/criarCliente',
+    {
+      schema: {
+        summary: 'Criar Cliente',
+        tags: ['clientes'],
+        body: clienteSchema,
+        response: {
+          200: { description: 'Cliente criado com sucesso', type: 'object' },
+        },
+      },
+    },
+    criarCliente,
+  );
+  fastify.get(
+    '/listarClientes',
+    {
+      schema: {
+        summary: 'Lista todos os clientes cadastrados',
+        tags: ['clientes'],
+        response: {
+          200: { type: 'array', items: { type: 'object' } },
+        },
+      },
+    },
+    listarClientes,
+  );
 }
